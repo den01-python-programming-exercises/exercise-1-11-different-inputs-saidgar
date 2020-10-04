0f0bc1df70ecf7d11bc217d2c99fd234ee01a973
saidgar <sebast45ian@gmail.com>
2020-10-04 22:08:05 +0200

    dfgssfdh

--- src/exercise.py
+++ src/exercise.py
@@ -3,11 +3,14 @@ def main():
     input1 = input("Give a string:")
     input2 = int(input("Give an integer:"))
     input3 = float(input("Give a float:"))
-    input4 = bool(input("Give a boolean:"))
+    input4 = input("Give a boolean:")
+    result = False
+    if input4 == "True":
+        result = True
     print("You gave the string " + str(input1))
     print("You gave the integer " + str(input2))
     print("You gave the float " + str(input3))
-    print("You gave the boolean " + str(input4))
+    print("You gave the boolean " + str(result))
 
 if __name__ == '__main__':
     main()
