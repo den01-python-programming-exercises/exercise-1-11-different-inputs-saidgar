51cf53999aa7bdbbdb8d6ce4a2dba176424dfa47
saidgar <sebast45ian@gmail.com>
2020-10-04 21:50:44 +0200

    sfdag

--- src/exercise.py
+++ src/exercise.py
@@ -5,10 +5,12 @@ def main():
     print("Give an integer:")
     input2 = int(input())
     print("Give a float:")
+    input3 = int(input())
     print("Give a boolean:")
     input4 = bool(input())
     print("You gave the string " + str(input1))
     print("You gave the integer " + str(input2))
+    print("You gave the float " + str(input3))
     print("You gave the boolean " + str(input4))
 
 if __name__ == '__main__':
